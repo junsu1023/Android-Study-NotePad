0ea039a43ac12088efeb1e999c7b585743cacc5e
kimjunsu <12jangdokg55@gmail.com>
2024-09-20 23:25:33 +0900

    modify NotePadDataModel - id: type is Int to Long?

--- domain/src/main/java/com/example/domain/model/NotePadModel.kt
+++ domain/src/main/java/com/example/domain/model/NotePadModel.kt
@@ -3,7 +3,7 @@ package com.example.domain.model
 import java.util.Date
 
 data class NotePadModel(
-    val id: Int = 0,
+    val id: Long? = null,
     val updateTime: Date = Date(System.currentTimeMillis()),
     val title: String? = null,
     val content: String? = null
