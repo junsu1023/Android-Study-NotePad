8c3218d89faa7a0a785910e2e737e899dd1a5882
kimjunsu <12jangdokg55@gmail.com>
2024-09-20 23:27:05 +0900

    Revert "modify NotePadDataModel - id: type is Int to Long?"
    This reverts commit 0ea039a43ac12088efeb1e999c7b585743cacc5e.

--- domain/src/main/java/com/example/domain/model/NotePadModel.kt
+++ domain/src/main/java/com/example/domain/model/NotePadModel.kt
@@ -3,7 +3,7 @@ package com.example.domain.model
 import java.util.Date
 
 data class NotePadModel(
-    val id: Long? = null,
+    val id: Int = 0,
     val updateTime: Date = Date(System.currentTimeMillis()),
     val title: String? = null,
     val content: String? = null
